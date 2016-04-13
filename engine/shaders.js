b364efe7d0af304749c62f8916e007ef12d650b7
DWinterbottom <purpleplainsofeternity@hotmail.com>
2016-04-13 01:48:47 +0100

    Fixing the shaders.
    I was going to do it tomorrow, but I got started and didn't stop.
    They compile now, at least. Mostly silly syntax errors, and forgetting to put the functions in order.
    Specified low precision for the colours, as this is designed to run on my laptop!

--- engine/shaders.js
+++ engine/shaders.js
@@ -7,25 +7,12 @@ varying float vHue;
 varying float vColourise;
 uniform sampler2D uTexture;
 
-void main(void)
+lowp vec4 GetHSL(vec4 v4RGBA)
 {
-    vec4 texColour = texture2D(uTexture, vTextureCoordinates);
-    if (uColourise == 0.0)
-    {
-        gl_FragColor = texColour;
-    }
-    else
-    {
-        vec4 v4HSLA = GetHSL(texColour);
-        fHueDistance = mod(0.5+(v4HSLA.x - vHue), 1.0) - 0.5;
-        float fAdjustedHue = mod(fTexHue + (fHueDistance * vColourise), 1.0);
-        v4HSLA.x = fAdjustedHue;
-        gl_FragColor = FromHSL(v4HSLA);
-    }
-vec4 GetHSL(vec4 v4RGBA)
-{
-    float fMax = max(v4RGBA.x, v4RGBA.y, v4RGBA.z);
-    float fMin = min(v4RGBA.x, v4RGBA.y, v4RGBA.z);
+    precision lowp float;
+
+    float fMax = max(v4RGBA.x, max(v4RGBA.y, v4RGBA.z));
+    float fMin = min(v4RGBA.x, min(v4RGBA.y, v4RGBA.z));
     
     float fTexLightness = 0.5*(fMax + fMin);
     
@@ -40,7 +27,7 @@ vec4 GetHSL(vec4 v4RGBA)
     }
     else
     {
-        fTexSaturation = fChroma / (1- abs(2*fTexLightness - 1));
+        fTexSaturation = fChroma / (1.0- abs(2.0*fTexLightness - 1.0));
     }
     
     float fBaseHue = 0.0;
@@ -65,10 +52,15 @@ vec4 GetHSL(vec4 v4RGBA)
     
     float fHueDistance = mod((vHue - fTexHue)+0.5, 1.0) - 0.5;
     
-    return vec4(fTexHue, fTexSaturation, fTexLightness, v4RGBA.w);
+    vec4 ret = vec4(fTexHue, fTexSaturation, fTexLightness, v4RGBA.w);
+    return ret;
 }
-vec4 FromHSL(vec4 v4HSLA)
+
+lowp vec4 FromHSL(vec4 v4HSLA)
 {
+    
+    precision lowp float;
+    
     float fChroma = 0.0;
     if (v4HSLA.y < 0.005)
     {
@@ -76,29 +68,29 @@ vec4 FromHSL(vec4 v4HSLA)
     }
     else 
     {
-        fChroma = (1 - abs(2 * v4HSLA.z - 1)) * v4HSLA.y;
+        fChroma = (1.0 - abs(2.0 * v4HSLA.z - 1.0)) * v4HSLA.y;
     }
     
-    vec3 intermediates = vec3(0.0,0.0,0.0)
-    float fSecondChroma = fChroma * (1 - abs(mod(v4HSLA.x * 6, 2.0) - 1));
-    float fSection = v4HSLA.x * 6
-    if (fSection < 1)
+    vec3 intermediates = vec3(0.0,0.0,0.0);
+    float fSecondChroma = fChroma * (1.0 - abs(mod(v4HSLA.x * 6.0, 2.0) - 1.0));
+    float fSection = v4HSLA.x * 6.0;
+    if (fSection < 1.0)
     {
         intermediates = vec3(fChroma, fSecondChroma, 0.0);
     }
-    else if (fSection < 2)
+    else if (fSection < 2.0)
     {
         intermediates = vec3(fSecondChroma, fChroma, 0.0);
     }
-    else if (fSection < 3)
+    else if (fSection < 3.0)
     {
         intermediates = vec3(0.0, fChroma, fSecondChroma);
     }
-    else if (fSection < 4)
+    else if (fSection < 4.0)
     {
         intermediates = vec3(0.0, fSecondChroma, fChroma);
     }
-    else if (fSection < 5)
+    else if (fSection < 5.0)
     {
         intermediates = vec3(fSecondChroma, 0.0, fChroma);
     }
@@ -106,10 +98,28 @@ vec4 FromHSL(vec4 v4HSLA)
     {
         intermediates = vec3(fChroma, 0.0, fSecondChroma);
     }
-    float fMin = v4HSLA.z - (0.5 * fChroma)
-    return vec4(intermediates.x + fMin, intermediates.y + fMin, intermediates.z + fMin, v4HSLA.w)
+    float fMin = v4HSLA.z - (0.5 * fChroma);
+    vec4 ret = vec4(intermediates.x + fMin, intermediates.y + fMin, intermediates.z + fMin, v4HSLA.w);
+    return ret;
 }
-}`
+
+void main(void)
+{
+    vec4 texColour = texture2D(uTexture, vTextureCoordinates);
+    if (vColourise == 0.0)
+    {
+        gl_FragColor = texColour;
+    }
+    else
+    {
+        vec4 v4HSLA = GetHSL(texColour);
+        float fHueDistance = mod(0.5+(v4HSLA.x - vHue), 1.0) - 0.5;
+        float fAdjustedHue = mod(v4HSLA.x + (fHueDistance * vColourise), 1.0);
+        v4HSLA.x = fAdjustedHue;
+        gl_FragColor = FromHSL(v4HSLA);
+    }
+}
+`
 
 
 shaders.vertex = `
@@ -117,7 +127,7 @@ attribute vec2 aVertexPosition;
 attribute vec2 aTetureCoordinates;
 attribute float aHue;
 attribute float aColourise;
-varying vec2 vTextureCoordinates
+varying vec2 vTextureCoordinates;
 varying float vHue;
 varying float vColourise;
 uniform float uLayerHeight;
@@ -129,5 +139,5 @@ void main(void)
     vTextureCoordinates = aTetureCoordinates;
     vHue = aHue;
     vColourise = aColourise;
-}
+}`
 
